3e0c3c9c42da63b68dbbe668ce7c33b45bac73a1
daniel <danielcampelo159@gmail.com>
2025-08-05 01:03:33 -0300

    a

--- src/script.js
+++ src/script.js
@@ -1,27 +1,29 @@
-                                        $( document ).ready(function() {
-    
+$(document).ready(function () {
+
     var envelope = $('#envelope');
     var btn_open = $("#open");
     var btn_reset = $("#reset");
-    
-    envelope.click( function() {
+
+    envelope.click(function () {
         open();
     });
-    btn_open.click( function() {
+    btn_open.click(function () {
         open();
     });
-    btn_reset.click( function() {
+    btn_reset.click(function () {
         close();
     });
 
     function open() {
         envelope.addClass("open")
-           .removeClass("close");
+            .removeClass("close");
     }
     function close() {
         envelope.addClass("close")
-           .removeClass("open");
+            .removeClass("open");
     }
-   
-}); 
-            
+
+});
+
+    const audio = new Audio();
+    audio.src = "./midia/Djavan.mp3"
